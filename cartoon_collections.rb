c48bc3097a7ffea6147b139ea2ecbcd56336eaa5
Bernardngari <bernardngari.ke@gmail.com>
2022-08-19 13:07:00 +0300

    commit tree 85fc8d0d46f4990e9afeb594dc3bdec144a322a4

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves array_of_dwarfes
+puts array_of_dwarfes.map.with_index(1){|dwarf,i| "#{i}. #{dwarf}"}
+
+
+end
+#uncomment line 8 to test.
+#roll_call_dwarves(["dwarf1", "dwarf2", "dwarf3"])
+
+def summon_captain_planet array_of_planeteer
+	output = array_of_planeteer.map{|planeteer| planeteer.capitalize << "!"}
+  return output
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+#uncomment line 16 to test code.
+#puts summon_captain_planet(["planeteer1", "planeteer2", "planeteer3"])
+
+def long_planeteer_calls array_of_calls
+	if array_of_calls.any? {|call| call.size > 4}
+		return true
+	elsif array_of_calls.all?{|call| call.size <= 4}
+	return false
+	end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+#puts long_planeteer_calls(["dwar", "dwar", "dwar"])
 
-def find_the_cheese# code an argument here
+def find_the_cheese items
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+	items.find do |maybe_cheese|
+		cheese_types.include?(maybe_cheese)
+	end
 end
+
+puts find_the_cheese(["tomato soup", "cheddar", "oyster crackers", "gouda"])
